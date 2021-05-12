44d15f2f30e05f0157c54ca5f5b35f30a15060eb
Elina Muradova <muradovaelina@gmail.com>
2021-05-12 17:55:59 +0200

    two

---
+++ app/models/restaurant.rb
@@ -0,0 +1,9 @@
+class Restaurant < ApplicationRecord
+  CATEGORY = %w(chinese italian japanese french belgian)
+
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORY }
+  has_many :reviews, dependent: :destroy
+end
